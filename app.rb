1d7a73d1951398b834f7d6b867c549c2bf308393
Jelgar <jonathanelgardiner@gmail.com>
2015-12-07 17:13:21 +0000

    introduced params

--- app.rb
+++ app.rb
@@ -13,7 +13,13 @@ get '/test' do
   'dumdedumdedum'
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
